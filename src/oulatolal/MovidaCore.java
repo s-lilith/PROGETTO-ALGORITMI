dbc5c9e3a5acb05105a15a9288aeae7e2f7fe38c
user0x7419553 <43680130+user0x7419553@users.noreply.github.com>
2020-11-06 22:44:37 +0100

    Update MovidaCore.java

--- src/oulatolal/MovidaCore.java
+++ src/oulatolal/MovidaCore.java
@@ -1,11 +1,13 @@
 package oulatolal;
 
 import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import commons.Collaboration;
@@ -18,6 +20,7 @@ import commons.MovidaFileException;
 import commons.Movie;
 import commons.Person;
 import commons.SortingAlgorithm;
+import oulatolal.Graph;
 
 public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMovidaCollaborations{
 
@@ -25,17 +28,19 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 	StrutturaDati movies2= new LinkedList();
 	Sorting sorting_algorithm = new BubbleSort();
 	Sorting sorting_algorithm2 = new MergeSort();
+	Graph collaborationgraph = new Graph();
+
 	
 	@Override
 	public Person[] getDirectCollaboratorsOf(Person actor) {
 		// TODO Auto-generated method stub
-		return null;
+		return collaborationgraph.getCollaborators(actor);
 	}
 
 	@Override
 	public Person[] getTeamOf(Person actor) {
 		// TODO Auto-generated method stub
-		return null;
+		return collaborationgraph.getTeam(actor);
 	}
 
 	@Override
@@ -196,74 +201,6 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 		return null;
 	}
 	
-	
-	
-	
-	
-	/**
-	 * questa parte del codice serve per leggere valori dal file
-	 * e poterli poi salvare in una struttura dati
-	 * creo delle variabili temporanee che mi servono per potere salvare i dati 
-	 * e a seconda della necessità poi vengono convertiti.
-	 *  1)Inizio con la lettura della stringa titolo, t;
-	 *  
-	 *  2)Leggo poi l'anno. Essendo year: xxxx (valore numerico) una stringa,
-	 *  dovevo "rimuovere" ciò che non era necessario dalla stringa, per poi 
-	 *  castarla in un intero. 
-	 *  
-	 *  3)Per il cast, che è un vettore di persone, ho pensato di usare le 
-	 *  virgole per avere una lunghezza. Nel senso, se ho 4 attori nel cast,
-	 *  questi verranno delimitati da 3 virgole, quindi la lunghezza sarà
-	 *  virgole+1. 
-	 *  Quindi una volta letta la stringa degli attori:
-	 *  	-conto le virgole
-	 *  	-creo la lunghezza del vettore persone[virgole+1]
-	 *  	-uso un vettore d'appoggio stringa, per separare i valori salvati
-	 *  		nella stringa, tramite string.split(",");
-	 *  	-assegno poi al vettore persona iesimo, il valore iesmio
-	 *  		del vettore stringa. 
-	 *  
-	 *  4) Alla fine per leggere l'ultimo valore relativo ai voti,
-	 *  è stata eseguita la stessa procedura fatta con l'anno, dove veniva
-	 *  letta la stringa, successivamente usando replaceAll, trim e replaceAll
-	 *  veniva "pulita" dai caratteri e poi castata come intero.
-	 */
-		
-		
-		//////////////////////////////////
-		/**
-		 * il problema che mi sto ponendo è se usare bufferreader o scanner
-		 * se scanner consuma i caratteri letti e li "brucia"
-		 * pensavo di usare un count, in modo tale d'avere un'idea di quanto
-		 * deve skippare, ma credo che scanner consumi
-		 * e forse quindi devo usare il bufferreader 
-		 * 
-		 * quindi forse bisogna fare una versione alternativa con buffer
-		 */
-		
-		
-
-		
-		////////////////////////
-		/**
-		 * Uso il bufferreader, invece dello scanner, perché sembra che con 
-		 * scanner io bruci le righe e non riesca ad utilizzarle (vedo se 
-		 * cambiare scanner e tenere tutto con buffer) 
-		 * Uso il bufferedReader, per poter contare il numero delle righe
-		 * nel file. In modo tale da poter costruire un vettore movie, di 
-		 * numero di righe nel file / il numero delle componenti
-		 * ovvero, vedendo la struttura del file del testo so che c'è
-		 * un titolo
-		 * un anno
-		 * un direttore
-		 * un cast (si suppone che i vari nomi degli attori vengano tutti 
-		 * 	scritti in una riga sola).
-		 *  il voto
-		 *  e uno spazio bianco
-		 *  (Quindi sei righe).
-		 *
-		 * 
-		 */
 		
 	@Override
 	public void loadFromFile(File f) {
@@ -286,7 +223,6 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println(lines);
 			/////////////////////////////////////////////////////////
 			int lunghezzaMovieVettore = lines/6;
 			//creo una variabile vettore movie per poter salvare i film letti dal file
@@ -298,53 +234,37 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 						String title_string = scan.nextLine();
 						title_string = title_string.replaceAll("Title:", " "); 
 				  
-						// Remove extra spaces from the beginning 
-						// and the ending of the string 
 						title_string = title_string.trim(); 
 			  
-						// Replace all the consecutive white 
-						// spaces with a single space 
-						title_string = title_string.replaceAll(" +", " "); 
+						title_string = title_string.replaceAll(" + ", " "); 
 				
 						String year_string = scan.nextLine();
-						// Replacing every non-digit number 
-						// with a space(" ") 
+						
 						year_string = year_string.replaceAll("Year:", " "); 
-		  
-						// Remove extra spaces from the beginning 
-						// and the ending of the string 
+		 
 						year_string = year_string.trim(); 
 		  
-						// Replace all the consecutive white 
-						// spaces with a single space 
-						year_string = year_string.replaceAll(" +", " "); 
+						year_string = year_string.replaceAll(" + ", " "); 
 		  
 						int year_integer = Integer.parseInt(year_string); ///<--- per castare
+						
 						String director_string = scan.nextLine();
+						
 						director_string = director_string.replaceAll("Director:", " "); 
 				  
-						// Remove extra spaces from the beginning 
-						// and the ending of the string 
 						director_string = director_string.trim(); 
 			  
-						// Replace all the consecutive white 
-						// spaces with a single space 
-						director_string = director_string.replaceAll(" +", " "); 
+						director_string = director_string.replaceAll(" + ", " "); 
+						
 						Person director = new Person(director_string);
 				
-						///leggiamo il cast
 						String cast_string = scan.nextLine();
 				
-						// with a space(" ") 
-						cast_string = cast_string.replaceAll("Cast:", " "); 
+						cast_string = cast_string.replaceAll("Cast: ", " "); 
 		  
-						// Remove extra spaces from the beginning 
-						// and the ending of the string 
 						cast_string = cast_string.trim(); 
 		  
-						// Replace all the consecutive white 
-						// spaces with a single space 
-						cast_string = cast_string.replaceAll(" +", " "); 
+						cast_string = cast_string.replaceAll(" + ", " "); 
 				
 						String[] cast_array;
 				
@@ -356,29 +276,22 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 							if(cast_string.charAt(i) == ',') 
 								commas++;
 							}
-						System.out.println(commas);
 						//creo il vettore di lunghezza commas+1 per il cast
 						Person cast[] = new Person[commas+1];
-						cast_array = cast_string.split(","); //<--- serve per separare i valori 
-						//dalle virgole
+						cast_array = cast_string.split(", "); //<--- serve per separare i valori 
+						//dalle virgole. E' importante che sia virgola e spazio, perché altrimenti
+						//ci sono problemi con l'hashcode 
 			
-				
-						//per l'iesima persona del cast, assegno l'iesimo valore salvato 
-						//nel vettore stringa
 						for (int i=0; i<commas+1; i=i+1) {
 							cast[i] = new Person(cast_array[i]);
 						}
-						///leggo i valori riguardo ai voti del film
+					
 						String vote_string = scan.nextLine();
 						vote_string = vote_string.replaceAll("Votes:", " "); 
 				  
-						// Remove extra spaces from the beginning 
-						// and the ending of the string 
 						vote_string = vote_string.trim(); 
 			  
-						// Replace all the consecutive white 
-						// spaces with a single space 
-						vote_string = vote_string.replaceAll(" +", " "); 
+						vote_string = vote_string.replaceAll(" + ", " "); 
 			       
 						int vote_integer = Integer.parseInt(vote_string);
 						String whitespace = scan.nextLine();
@@ -392,6 +305,7 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 			//sulla strutturadati (a seconda del caso, linked list o btree)
 			for (int i=0; i<lunghezzaMovieVettore; i=i+1) {
 				movies.insert(movie[i]);
+				collaborationgraph.insertMovieGraph(movie[i]);
 			}
 			scan.close();
 		}
@@ -431,15 +345,9 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 		// TODO Auto-generated method stub
 		int conta = 0;
 		Movie [] result = movies.getMovies();
-		//ok, la dimensione del vettore è giusta
 		if (result!=null) {
-			for (int i=0; i<result.length; i=i+1) {
-				//per ogni film i-esimo, si prende il vettore cast
-				//si prende la lunghezza e la si aggiunge a conta, così
-				//per ogni film
+			for (int i=0; i<result.length; i=i+1) {	
 				conta = result[i].getCast().length + conta;
-				//qui viene aggiunto il direttore del film 
-				//suppongo che sia uno
 				conta = conta+1;
 			}
 				
@@ -507,7 +415,7 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 	}
 
 	
-	//è necessario che ci sia il doppio controllo?
+
 	@Override
 	public Person[] getAllPeople() {
 		// TODO Auto-generated method stub
@@ -516,16 +424,16 @@ public class MovidaCore implements IMovidaDB, IMovidaSearch, IMovidaConfig, IMov
 		if (people!=null) {
 			for (int i=0; i<movies.getMovies().length; i=i+1) {
 				for (int j=0; j<movies.getMovies()[i].getCast().length;j=j+1) {
-				people[k] = movies.getMovies()[i].getCast()[j];
-				k=k+1;
-			}
+					people[k] = movies.getMovies()[i].getCast()[j];
+					k=k+1;
+					}
 			people[k] = movies.getMovies()[i].getDirector();
 			k=k+1;
-		}
+			}
 		return people;
 		}
 		return null;
 	}
-
+	
 
 	}	
