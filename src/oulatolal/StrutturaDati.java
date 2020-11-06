efebee213f49721da67e40a8b473664a10409f71
user0x7419553 <43680130+user0x7419553@users.noreply.github.com>
2020-11-06 22:53:53 +0100

    Update StrutturaDati.java

--- src/oulatolal/StrutturaDati.java
+++ src/oulatolal/StrutturaDati.java
@@ -5,7 +5,6 @@ import commons.Movie;
 //in questa classe vengono gestite le funzioni in comune dalle due strutture dati
 
 public abstract class StrutturaDati {
-	
 	public abstract void insert(Movie movie);
 	public abstract Movie searchMovie(Movie movie);
 	public abstract void delete(Movie movie);
