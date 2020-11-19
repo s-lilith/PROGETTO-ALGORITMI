c13add8d5ebc6bc3d227ca715b9735961af0b502
user0x7419553 <43680130+user0x7419553@users.noreply.github.com>
2020-11-19 18:52:57 +0100

    Update Graph.java

--- src/oulatolal/Graph.java
+++ src/oulatolal/Graph.java
@@ -11,135 +11,141 @@ import java.util.AbstractCollection;
 import commons.Collaboration;
 import commons.Movie;
 
+/**
+ * Aggiornamento del 26 ottobre 2020
+ * 	G = (V, E), quindi facciamo che i nodi del grafo siano le persone
+ * 	mentre gli Edges siano le collaborazioni.
+ * 
+ * Idea: Fare una lista per gli vertici e fare una lista di edges
+ *
+ * @author alpaca
+ *
+ */
 public class Graph {
-	 private HashMap<Person, ArrayList<Collaboration>> graph;
-	 Graph(){
-		 graph = new HashMap<Person, ArrayList<Collaboration> >();
-	 }
-	 
-	 public void addNode(Person p) {
-		 if (!graph.containsKey(p))
-		 graph.put(p, new ArrayList<Collaboration>());	 
-	 }
+	private HashMap<Person, ArrayList<Collaboration>> graph;
+	Graph(){
+		graph = new HashMap<Person, ArrayList<Collaboration> >();
+	}
 
-		 public void insertMovieGraph(Movie m) {
-		 for (int i=0; i<m.getCast().length; i=i+1) {
-			 for (int j=0; j<m.getCast().length; j=j+1) {
-				 if (m.getCast()[i].equals(m.getCast()[j])) {
-					 for (int k=0; k<m.getCast().length; k=k+1) {
-						 if (!(m.getCast()[i].equals(m.getCast()[k]))) {
-							 setCollaborations(m.getCast()[i], m.getCast()[k], m);
-							 }
-						 }
-					 }
-			 	}
-			 }
-	 	}
-	 
-	 
-	 public void setCollaborations(Person p, Person p1, Movie m) {
-	    if (!(graph.containsKey(p))) {
-	    	addNode(p);
-	    }
-	    if (!graph.containsKey(p1)) {
-	    	addNode(p1);
-	    }
-	    Collaboration collaboration = new Collaboration(p, p1);
-	    graph.get(p).add(collaboration);
-	    graph.get(p1).add(collaboration);
-	    collaboration.addMovie(m);
-	 }
+	public void addNode(Person p) {
+		if (!graph.containsKey(p))
+			graph.put(p, new ArrayList<Collaboration>());	 
+	}
 
-	
+
+	/**
+	 * Idea: aggiungere i nodi al grafo
+	 * controllare se ci sono collaborazioni, se ci sono, bene, altrimenti vengono aggiunte
+	 * (ricontrollareeee))
+	 * @param p
+	 * @param p1
+	 */
+	public void insertMovieGraph(Movie m) {
+		for (int i=0; i<m.getCast().length; i=i+1) {
+			for (int j=0; j<m.getCast().length; j=j+1) {
+				if (m.getCast()[i].equals(m.getCast()[j])) {
+					for (int k=0; k<m.getCast().length; k=k+1) {
+						if (!(m.getCast()[i].equals(m.getCast()[k]))) {
+							setCollaborations(m.getCast()[i], m.getCast()[k], m);
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	public void setCollaborations(Person p, Person p1, Movie m) {
+		if (!(graph.containsKey(p))) {
+			addNode(p);
+		}
+		if (!graph.containsKey(p1)) {
+			addNode(p1);
+		}
+		Collaboration collaboration = new Collaboration(p, p1);
+		graph.get(p).add(collaboration);
+		graph.get(p1).add(collaboration);
+		collaboration.addMovie(m);
+	}
+
 	public Person[] getCollaborators (Person actor) {
 		Person[] people = null;
 		ArrayList<Person> tmp = new ArrayList<>();
 		if (graph.get(actor)!=null) {
-		for (int i=0; i<graph.get(actor).size(); i=i+1) {
-			if (graph.get(actor).get(i).getActorA().equals(actor)) {
-				tmp.add(graph.get(actor).get(i).getActorB());
+			for (int i=0; i<graph.get(actor).size(); i=i+1) {
+				if (graph.get(actor).get(i).getActorA().equals(actor)) {
+					tmp.add(graph.get(actor).get(i).getActorB());
+				}
 			}
-		}
-		if (tmp!=null) {
-			people = new Person[tmp.size()];
-			for (int i=0; i<tmp.size(); i=i+1) {
-				people[i] = tmp.get(i);
+			if (tmp!=null) {
+				people = new Person[tmp.size()];
+				for (int i=0; i<tmp.size(); i=i+1) {
+					people[i] = tmp.get(i);
 				}
 			}
 		}
 		return people;
-	 }
-	 
-	private ArrayList<Person> starting_list (ArrayList<Person[]> collaborators, Person actor, ArrayList<Person> collaborators_list){
-		ArrayList<Person> tmp_people = new ArrayList<>();
-		for (Person[] p_iterator: collaborators) {
-			if (p_iterator!=null) {
-			for (int i=0; i<p_iterator.length; i=i+1) {
-				if (!(p_iterator[i].equals(actor)))
-				tmp_people.add(p_iterator[i]);
-				}
-			}
-		}
-		if (tmp_people!=null) {
-		tmp_people.removeAll(collaborators_list);	
-		}
-		return tmp_people;
-	}
-	
-	
-	
-	
-	////////////////////////////////////da guardare un po' meglio ///////////////////////// 
-	/**
-	La funzione funziona e se ho capito bene la consegna, fa quello che dovrebbe fare, però non mi convince la struttura del codice. 
-	*/
-	public Person [] getTeam(Person actor) {
-		ArrayList<Person []> tmp = new ArrayList<>();
-		ArrayList<Person> tmp_people = new ArrayList<>();
-		ArrayList<Person> people_team = new ArrayList<>();
-		Person [] collaborators = getCollaborators(actor);
-		ArrayList<Person> collaborators_list = new ArrayList<>();
-		Person[] teamOf = null;
- 		for (int i=0; i<collaborators.length; i=i+1) {
-			collaborators_list.add(collaborators[i]);
-		}
-		
-			for (int i=0; i<collaborators_list.size(); i=i+1) 
-			tmp.add(getCollaborators(collaborators_list.get(i)));
-			
-			tmp_people.addAll(starting_list(tmp, actor, collaborators_list));
-			if (tmp_people!=null) {
-				for (int i=0; i<tmp_people.size(); i=i+1) {
-					people_team.add(tmp_people.get(i));
-				}
-		
-				for (int i=0; i<tmp_people.size(); i=i+1) {
-					collaborators_list.add(tmp_people.get(i));
-				}
-				for (int i=0; i<people_team.size(); i=i+1) {
-					tmp.add(getCollaborators(people_team.get(i)));
-					}
-				}
-			
-			tmp_people.addAll(starting_list(tmp, actor, collaborators_list));
-			
-			tmp_people.removeAll(collaborators_list);
-			
-			for (int i=0; i<tmp_people.size(); i=i+1) {
-				people_team.add(tmp_people.get(i));
-			}
-			if (people_team!=null) {
-				people_team.add(actor);
-				teamOf = new Person[people_team.size()];
-				for (int i=0; i<teamOf.length; i=i+1) {
-					teamOf[i] = people_team.get(i);
-				}
-			}
-		
-		return teamOf;
 	}
 
-	
-	
-	 
+
+
+	private ArrayList<Person> getCollaborators_arrayList (Person actor){
+		ArrayList<Person> collaboration_arrayList = new ArrayList<>();
+		if (graph.get(actor)!=null) {
+			for (int i=0; i<graph.get(actor).size(); i=i+1) {
+				if (graph.get(actor).get(i).getActorA().equals(actor)) {
+					collaboration_arrayList.add(graph.get(actor).get(i).getActorB());
+				}
+			}
+		}
+		return collaboration_arrayList;
+	}
+
+
+	public Person[] getTeam (Person actor) {
+		boolean done = false;
+		ArrayList<Person> collaborators = new ArrayList<>();
+		collaborators = getCollaborators_arrayList(actor);
+		ArrayList<Person> team = new ArrayList<>();
+		team = getCollaborators_arrayList(actor);
+		ArrayList<Person> tmp_array = new ArrayList<>();
+		ArrayList<Person> different = new ArrayList<>();
+		Person[] team_result = null;
+		while (!(done)) {
+			for (int i=0; i<collaborators.size(); i=i+1) {
+				Person tmp = collaborators.get(i);
+				tmp_array = getCollaborators_arrayList(tmp);
+				for (int j=0; j<tmp_array.size(); j=j+1) {
+					Person tmp_collaborator = tmp_array.get(j);
+					if (!(team.contains(tmp_collaborator))) {
+						team.add(tmp_collaborator);
+						different.add(tmp_collaborator);
+					}
+				}	
+			}
+			collaborators.removeAll(collaborators);
+			if (collaborators.isEmpty()) {
+				if (!(different.isEmpty())) {
+					collaborators.addAll(different);
+					different.removeAll(different);
+				} else {
+					done = true;
+				}
+			}
+		}
+
+		if (!(team.isEmpty())) {
+			team_result = new Person[team.size()];
+			for (int i=0; i<team.size(); i=i+1) {
+				team_result[i] = team.get(i);
+			}
+		}
+		return team_result;
+	}
+
+
+
+
+
+
 }
