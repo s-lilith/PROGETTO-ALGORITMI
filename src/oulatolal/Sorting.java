325679d46b904f585f4ecaa26bd5d39143206a4a
user0x7419553 <43680130+user0x7419553@users.noreply.github.com>
2020-12-30 16:34:41 +0100

    Delete Sorting.java

--- src/oulatolal/Sorting.java
+++
@@ -1,9 +0,0 @@
-package oulatolal;
-
-import commons.Movie;
-
-public abstract class Sorting {
-	public abstract void sort(Movie[] movieArray);
-	public abstract void sort(Movie[] movieArray, int n);
-
-}
