e58383b3c0dfc0978abe0e513c5cb8be12649e6b
user0x7419553 <43680130+user0x7419553@users.noreply.github.com>
2021-01-14 14:47:53 +0100

    Update ListaNonOrdinata.java

--- src/laloulato/ListaNonOrdinata.java
+++ src/laloulato/ListaNonOrdinata.java
@@ -1,16 +1,9 @@
 package laloulato;
 
-import java.util.ArrayList;
-
 import commons.Movie;
 
 public class ListaNonOrdinata extends StrutturaDati{
 	public NodeLinkedList head, tail;
-
-	/*public ListaNonOrdinata() {
-
-		makeEmpty();
-	}*/
 
 	public void makeEmpty() {
 
@@ -26,10 +19,9 @@ public class ListaNonOrdinata extends StrutturaDati{
 
 		NodeLinkedList new_node = new NodeLinkedList(m);
 
-		/* 3. Make next of new Node as head */
+		
 		new_node.next = head;
 
-		/* 4. Move the head to point to new Node */
 		head = new_node;
 	}
 
@@ -71,7 +63,7 @@ public class ListaNonOrdinata extends StrutturaDati{
 
 	public void deleteMovie(NodeLinkedList node, Movie m) throws EmptyLinkedListException {
 
-		//controllo se il nodo da eliminare è in testa
+		//controllo se il nodo da eliminare Ã¨ in testa
 		if(node.getMovie().getTitle().equals(m.getTitle())){
 			if (node.next== null){
 				System.out.println("List cannot be empty");
@@ -86,14 +78,14 @@ public class ListaNonOrdinata extends StrutturaDati{
 			return;
 		}
 
-		//se il nodo non è in testa
+		//se il nodo non Ã¨ in testa
 		NodeLinkedList prev= node;
 
 		while (prev.next.movie!= null && prev.next.movie!= m){
 			prev= prev.next;
 		}
 
-		//controllo se il nodo è presente nella lista
+		//controllo se il nodo Ã¨ presente nella lista
 		if (prev.next.movie== null){
 			System.out.println("Movie non present");
 			return;
@@ -107,16 +99,6 @@ public class ListaNonOrdinata extends StrutturaDati{
 	}
 
 	public NodeLinkedList search(NodeLinkedList n, Movie m){
-		/*if(isEmpty()) {
-			return null;
-		}
-		else {
-			if (n.getMovie().getTitle().equals(m.getTitle()))
-				//(n.getMovie().getTitle().equals(m.getTitle()))
-				return n;
-			else return search(n.next, m);
-		}*/
-
 
 		//salvo nodo head
 		NodeLinkedList tmp= head;
@@ -127,10 +109,9 @@ public class ListaNonOrdinata extends StrutturaDati{
 			i++;
 			tmp=tmp.next; //aggiorno
 		}
-		//se m non c'è
+		//se m non c'Ã¨
 		if (!tmp.movie.getTitle().equals(m.getTitle())) return null;
 		return search(tmp.next, m);
-
 
 	}
 
@@ -181,231 +162,5 @@ public class ListaNonOrdinata extends StrutturaDati{
 		}
 	}
 
-
-
-
-
-
-
-
-
-
-
-  /*private NodeLinkedList head, tail;
-
-  public ListaNonOrdinata() {
-
-  	makeEmpty();
-  }
-
-  public void makeEmpty() {
-
-  	head=tail=new NodeLinkedList();
-  }
-
-  public boolean isEmpty() {
-
-  	return(head==tail);
-  }
-
-  //inserisco film all'inizio dello lista se questa ? vuota
-  //altrimenti lo inserisco come nodo successivo
-  public void insertMovie(Movie m) {
-
-	  NodeLinkedList n = new NodeLinkedList();
-	  n.movie = m;
-	  n.next = null;
-
-	  if (head == null) {
-		  head = n;
-		  tail = n;
-		  head.prev = null;
-		  return;
-	  }
-	  if (n.compareTo(head.movie) < 1) {
-		  n.prev = null;
-		  head.prev = n;
-		  n.next = head;
-		  head = n;
-		  return;
-	  }
-
-	  if (n.compareTo(tail.movie) > 1) {
-		  n.prev = tail;
-		  tail.next = n;
-		  tail = n;
-		  return;
-	  }
-	  NodeLinkedList tmp = head.next;
-	  while ((tmp.movie.compareTo(n.movie)< 1)){
-		  tmp = tmp.next;
-		  n.prev = tmp.prev;
-		  tmp.prev = n;
-		  n.next = tmp;
-	  }
-
-
-  }
-  public void insert_Movie(Movie m) {
-	  if(isEmpty()) {
-	  //inserisco film nell'header
-	  head.setMovie(m);
-	  //Nodo con due riferimenti null
-	  NodeLinkedList newNode= new NodeLinkedList();
-	  //collego nodo a header
-	  newNode.setNext(head);
-	  head=newNode;
-	  }
-	  else {
-		  tail.setNext(new NodeLinkedList(m,null));
-		  tail=tail.getNext();
-	  }
-  }
-
-  //elimina partendo dall'ultimo nodo
-  public void deleteMovie() throws EmptyLinkedListException {
-	  Movie m=getLastFilm();
-	  //cerco fino ad arrivare al penultimo nodo
-	  NodeLinkedList temp=head;
-	  while (temp.getNext()!=tail)
-		  temp=temp.getNext();
-	  tail=temp;
-	  tail.setNext(null);
-  }
-
-  public Movie deleteFirstMovie() throws EmptyLinkedListException {
-	  Movie m= getFirstFilm();
-
-	  head=head.getNext();
-	  head.setMovie(null);
-	  return m;
-  }
-
-  public Movie getFirstFilm() throws EmptyLinkedListException {
-	  if(isEmpty())
-		  throw new EmptyLinkedListException();
-	  return head.getNext().getMovie();
-  }
-
-  public Movie getLastFilm() throws EmptyLinkedListException {
-	  if(isEmpty())
-		  throw new EmptyLinkedListException();
-	  return tail.getMovie();
-  }
-
-  //search
-  public NodeLinkedList search(NodeLinkedList n, Movie m){
-	  if(isEmpty()) {
-		  return null;
-	  }
-	  else {
-			if (n.getMovie().getTitle().equals(m.getTitle()))
-				return n;
-			else return search(n.next, m);
-	  }
-  }
-
-
-  //inserimento film in array
-  public void getMovies_array(NodeLinkedList n, Movie[] array, int index) {
-	  //TODO
-	  //controllo che la lista non sia vuota
-	  //se non è vuota faccio in modo che l'array sia vuoto
-
-	 /* if (n!=null){
-		array[index]=n.movie;
-		getMovies_array(n.next, array, checkIndex(array));
-	  //}
-	  if(n!= null){
-	  	array[index]=n.movie;
-	  	getMovies_array(n, array, checkIndex(array));
-	  	n=n.next;
-		} else return;
-
-	  }
-
-
-  //numero nodi nella lista
-	public int CountNodes(NodeLinkedList n){
-		int i=0;
-		if (n== null)
-			return 0;
-		else{
-
-			NodeLinkedList curr= this.head;
-			while (curr!= null){
-				i++;
-				curr=curr.next;
-			}
-			return i;
-		}
-	}
-
-	public int checkIndex(Movie[] array){
-  	int ind=0;
-  	for (int i=0; i< array.length; i++){
-  		if (array[i]==null)
-  			ind=i;
-	}
-		return ind;
-	}
-
-	@Override
-	public void insert(Movie movie) {
-		//  Auto-generated method stub
-		insertMovie(movie);
-	}
-
-	@Override
-	public void delete(Movie movie) {
-		//  Auto-generated method stub
-		try {
-			deleteMovie();
-		} catch (EmptyLinkedListException e) {
-			//  Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public Movie searchMovie(Movie movie){
-		//  Auto-generated method stub
-		NodeLinkedList n=search(this.head, movie);
-		if (n!= null)
-			return n.movie;
-		return null;
-
-	}
-	//questa funzione restituisce tutti i film presenti nella lista.
-	//Crea un vettore che ha lunghezza pari al numero di nodi presente nella lista
-	//chiama la funzioen getArray e restituisce il vettore con i film salvati
-
-	@Override
-	public Movie[] getMovies() {
-		//  Auto-generated method stub
-		ArrayList<Movie> res = new ArrayList<>();
-		Movie[] result = null;
-		NodeLinkedList n = this.head;
-		while (n!=null) {
-			res.add(n.movie);
-			n.setNext(n);
-		}
-		result = new Movie[res.size()];
-		for (int i=0; i<res.size(); i=i+1) {
-			result[i] = res.get(i);
-		}
-		return result;
-
-
-	}
-
-	@Override
-	public void clear() {
-		//  Auto-generated method stub
-		head=null;
-
-	}
-
-	*/
 
 }
